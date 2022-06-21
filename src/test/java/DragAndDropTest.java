76e9d83f407defe887f5e109febed4e0977ca873
dark11x <konets.sveta.2013@mail.ru>
2022-06-22 00:38:54 +0300

    рефакторинг теста

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -8,17 +8,22 @@ public class DragAndDropTest {
     @Test
     void shouldDragAtoB() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        //1 способ с использованием dragAndDropTo
         $("#column-a header").shouldHave(text("A"));
         $("#column-b header").shouldHave(text("B"));
 
-//       $("column-a").dragAndDropTo($("#column-b"));
-
-        sleep(2000);
-
-        actions().moveToElement($("#column-a")).clickAndHold().
-                moveByOffset(200,0).release().perform();
+        $("#column-a").dragAndDropTo("#column-b");
 
         $("#column-a header").shouldHave(text("B"));
         $("#column-b header").shouldHave(text("A"));
+
+        //2 способ с использованием moveToElement (не рабочий)
+        actions().moveToElement($("#column-a")).clickAndHold().
+                moveByOffset(200, 0).release().perform();
+
+        $("#column-a header").shouldHave(text("B"));
+        $("#column-b header").shouldHave(text("A"));
+
     }
 }
